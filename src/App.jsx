166c17f0f9a77371e88491f265a954efde0819de
manoj-bhandakar <developer.manojbhandarkar@gmail.com>
2026-03-13 08:02:08 +0530

    Revert "Fix Experience"

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,18 @@ import Footer from './components/Footer'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
 import CustomCursor from './utils/CursorAnimation'
-import Certifications from "./components/Cerifications"
-import Experience from './components/Experience'
 
 export default function App() {
   return (
     <div className='font-sora scroll-smooth overflow-x-hidden'>
-      <CustomCursor />
+      <CustomCursor/>
       <Navbar />
       <Home />
-      <Certifications />
-      <Projects />
       <Skills />
-      <Experience />
       <About />
+      <Projects />
       <Contact />
+
       <Footer />
     </div>
   )
